0664c4a416b5fce994d5b9e11e538303dce92355
Richard Ribeiro <65542005+richardribeiro1@users.noreply.github.com>
2022-04-10 12:10:16 -0300

    Update PCA9685_servo_control.ino

--- PCA9685_servo_control.ino
+++ PCA9685_servo_control.ino
@@ -25,7 +25,7 @@ void loop() {
   servoControl(1, 1700, 2300, 10); //define servo 1 from 1700 to 2300 with a speed of 10
   servoControl(0, 800, 2000, 5); //define servo 0 from 800 to 2000 with a speed of 5
   servoControl(1, 2300, 1000, 15); //define servo1 from 2300 to 1000 with a speed of 15
-  servoControl(0, 2000, 670, 7) //define servo from 2000 to 670 with a speed of 7
+  servoControl(0, 2000, 670, 7); //define servo from 2000 to 670 with a speed of 7
 
   /*
      this function is useful when you want to control de speed of movement using the PCA9685
