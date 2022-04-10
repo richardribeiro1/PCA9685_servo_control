9709586f565a92990bf20d18c34974161dee2a25
Richard Ribeiro <65542005+richardribeiro1@users.noreply.github.com>
2022-04-10 12:31:49 -0300

    Update PCA9685_servo_control.ino

--- PCA9685_servo_control.ino
+++ PCA9685_servo_control.ino
@@ -3,7 +3,6 @@
 
    Code to be used to Adafruit 16-Channel 12bit PWM/Servo Driver - I2C Interface - PCA9685
    www.github.com/adafruit/Adafuit-PWM-Servo-Driver-Library
-
 */
 
 #include <Adafruit_PWMServoDriver.h>
@@ -26,22 +25,6 @@ void loop() {
   servoControl(0, 800, 2000, 5); //define servo 0 from 800 to 2000 with a speed of 5
   servoControl(1, 2300, 1000, 15); //define servo1 from 2300 to 1000 with a speed of 15
   servoControl(0, 2000, 670, 7); //define servo from 2000 to 670 with a speed of 7
-
-  /*
-     this function is useful when you want to control de speed of movement using the PCA9685
-      pros:
-      uses only 2 pins (to communicate via I2C)
-      total control of the movement speed
-
-      cons:
-      must know the actual or last position of the servo.
-
-      Depending of the aplication you can use the servo library <servo.h>
-      and use readMicroseconds() to read the actual position.
-      For this case, check my github.
-      www.github.com/richardribeiro1
-  */
-  
 }
 
 //Homing function is important to set a default position
@@ -50,13 +33,8 @@ void homing() {
   delay(500); //waiting position
   pwm.writeMicroseconds(1, 1700);
   delay(500); //waiting position
-  pwm.writeMicroseconds(2, 2000);
-  delay(500); //waiting position
-  pwm.writeMicroseconds(3, 1000);
-  delay(500); //waiting position
 }
 
-//servoControl function is used to control every servo in the PCA9685
 void servoControl(int servonum, int posInitial, int posFinal, int speed) {
   int position;
   position = posInitial;
